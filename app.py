872ee0de900c737c6e4216cafbcdeb6032007c86
dablu <dablu10jan@gmail.com>
2025-04-20 23:00:15 +0530

    login page created

--- app.py
+++ app.py
@@ -1,47 +1,51 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 from common import success_responce, error_responce
 from logger import LOGGER
+from dotenv import load_dotenv
+import os
+from datetime import timedelta
 
+load_dotenv()
+_secreate_key = os.getenv('SECRET_KEY')
+_debug_mode = os.getenv('DEBUG', 'False')
 
 app = Flask(__name__)
+app.secret_key = _secreate_key
+app.permanent_session_lifetime = timedelta(seconds=10)
 
-@app.route("/<num>")
-def home(num):
-    return render_template('index.html', num= num)
+@app.route('/')
+def home():
+    return render_template('home.html')
 
-@app.route('/login', methods = ['POST', 'GET'])
+@app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
+        session.permanent = True
         _user = request.form['nm']
-        return redirect(url_for('user', usr = _user))
+        session['user'] = _user
+        flash(f'Welcome, {_user}!', 'success') 
+        return redirect(url_for('user'))
     else:
         return render_template('login.html')
+    
+@app.route("/logout")
+def logout():
+    if "user" in session:
+        flash('You have been logged out!', 'info')
+        session.pop('user', None)
+    return redirect(url_for('login'))
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
-
-# @app.route("/health", methods = ['GET'])
-# def health():
-#     LOGGER.info(f"health route started...")
-#     try:
-#         return success_responce(message='home call successfully')
-#     except Exception as e:
-#         LOGGER.info(f"error occured : {str(e)},", exc_info=True)
-#         raise e
-
-# @app.route("/home/<name>", methods = ['GET'])
-# def user(name):
-#     return f"Hello! {name}"
-
-# @app.route("/admin", methods = ['GET'])
-# def admin():
-#     return redirect(url_for('home'))
+@app.route("/user")
+def user():
+    if 'user' in session:
+        user = session['user']
+        return render_template('user.html', user=user)
+    else:
+        return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
         port=8080,
-        debug=True
+        debug=_debug_mode
     )
-
